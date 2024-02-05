2af7c894aea821d9b646e37bae054ee1db24dc37
LUIZ GALESSO <lgalesso@gmail.com>
2024-02-05 20:19:20 -0300

    Primeiro commit

---
+++ src/main/java/com/fase2/techchallenge/fiap/ApiRestauranteApplication.java
@@ -0,0 +1,13 @@
+package com.fase2.techchallenge.fiap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestauranteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestauranteApplication.class, args);
+    }
+
+}
