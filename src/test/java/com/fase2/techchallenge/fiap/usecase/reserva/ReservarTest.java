f620c25d4cbe7680ea7e802cea056d934a313290
Thiago <thiagoribeirods@outlook.com>
2024-03-21 17:34:42 -0300

    reserva gateway com tests e it

--- src/test/java/com/fase2/techchallenge/fiap/usecase/reserva/ReservarTest.java
+++ src/test/java/com/fase2/techchallenge/fiap/usecase/reserva/ReservarTest.java
@@ -3,7 +3,9 @@ package com.fase2.techchallenge.fiap.usecase.reserva;
 import com.fase2.techchallenge.fiap.entity.reserva.model.Reserva;
 import com.fase2.techchallenge.fiap.infrastructure.reserva.controller.dto.ReservaInsertDTO;
 import com.fase2.techchallenge.fiap.infrastructure.reserva.utils.ReservaHelper;
+import com.fase2.techchallenge.fiap.usecase.exception.BussinessErrorException;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -11,7 +13,7 @@ import org.mockito.MockitoAnnotations;
 
 import java.time.LocalDateTime;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -52,5 +54,26 @@ public class ReservarTest
         assertThat(resultado).isInstanceOf(Reserva.class);
         assertThat(resultado.getId()).isNotNull();
         assertThat(resultado.getDataHoraFinal()).isEqualTo(reserva.getDataHoraFinal());
+    }
+
+    @Test
+    void deveGerarExcecao_QuandoMesaNaoAtiva() {
+
+        //Arrange
+        LocalDateTime dataInicio = LocalDateTime.of(2024, 3, 20, 11, 30);
+        ReservaInsertDTO reservaInsertDTO = new ReservaInsertDTO(
+                1L,
+                2L,
+                "joao.silva@example.com",
+                dataInicio,
+                2
+        );
+        Reserva reserva = ReservaHelper.gerarReserva(1L, dataInicio, dataInicio.plusHours(reservaInsertDTO.getQuantidadeHoras()), "RESERVADO");
+        when(reservar.execute(any(ReservaInsertDTO.class))).thenReturn(reserva);
+
+        //Assert
+        assertThatThrownBy(() -> reservar.execute(reservaInsertDTO))
+                .isInstanceOf(BussinessErrorException.class)
+                .hasMessage("Mesa não está disponível para ser reservada.");
     }
 }
