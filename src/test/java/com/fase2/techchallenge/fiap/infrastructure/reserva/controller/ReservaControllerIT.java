7a6e04fd6f988f8b0d87381857c84b436e66d9dc
Thiago <thiagoribeirods@outlook.com>
2024-03-23 12:34:09 -0300

    perfil de tests e cucumber adicionado

--- src/test/java/com/fase2/techchallenge/fiap/infrastructure/reserva/controller/ReservaControllerIT.java
+++ src/test/java/com/fase2/techchallenge/fiap/infrastructure/reserva/controller/ReservaControllerIT.java
@@ -17,6 +17,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -27,6 +28,7 @@ import static io.restassured.RestAssured.when;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureTestDatabase
+@ActiveProfiles("test")
 public class ReservaControllerIT {
 
     @Autowired
--- src/test/java/com/fase2/techchallenge/fiap/usecase/reserva/RealizarCheckinIT.java
+++ src/test/java/com/fase2/techchallenge/fiap/usecase/reserva/RealizarCheckinIT.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
 
@@ -19,6 +20,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 @SpringBootTest
 @AutoConfigureTestDatabase
 @Transactional
+@ActiveProfiles("test")
 public class RealizarCheckinIT {
 
     @Autowired
